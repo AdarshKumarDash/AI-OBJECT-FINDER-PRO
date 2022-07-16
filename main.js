0b7114a85d149d02137524a4ae40bc7db03778a3
Adarsh Kumar <86519736+AdarshKumarDash@users.noreply.github.com>
2022-07-16 17:59:24 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,23 @@
-function preload() {
-    video = createVideo("video.mp4");
-}
-
+status = "";
+objectinput = "";
 
 function setup() {
     canvas = createCanvas(640, 400);
     canvas.center();
+    video.createCapture(VIDEO);
+    video.center();
     video.hide();
+}
+
+function startdetect() {
+    objectDetection = ml5.objectDetector("cocossd", modelLoaded);
+    document.getElementById("status").innerHTML = "Detecting Object";
+    objectinput = document.getElementById("objectinp").value;
+}
+
+function modelLoaded() {
+    console.log("Model Loaded");
+    status = true;
 }
 
 function draw() {
